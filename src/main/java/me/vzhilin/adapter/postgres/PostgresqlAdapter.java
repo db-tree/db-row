7dbb192a5aec290b5fc7d72b6e4a831108a17423
vzhilin <zhilin.vladimir.nic@gmail.com>
2019-10-16 20:15:09 +0300

    postgresql support

---
+++ src/main/java/me/vzhilin/adapter/postgres/PostgresqlAdapter.java
@@ -0,0 +1,33 @@
+package me.vzhilin.adapter.postgres;
+
+import me.vzhilin.adapter.DatabaseAdapter;
+import me.vzhilin.adapter.ValueConverter;
+import me.vzhilin.catalog.Table;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public final class PostgresqlAdapter implements DatabaseAdapter {
+    private final ValueConverter conv;
+
+    public PostgresqlAdapter() {
+        this.conv = new PostgresqlValueConverter();
+    }
+
+    @Override
+    public ValueConverter getConverter() {
+        return conv;
+    }
+
+    @Override
+    public String qualifiedTableName(Table table) {
+        return table.getName();
+    }
+
+    @Override
+    public String defaultSchema(Connection conn) throws SQLException {
+        return new QueryRunner().query(conn, " select current_schema()", new ScalarHandler<>());
+    }
+}
--- src/main/java/me/vzhilin/adapter/postgres/PostgresqlValueConverter.java
+++ src/main/java/me/vzhilin/adapter/postgres/PostgresqlValueConverter.java
@@ -1,4 +1,4 @@
-package me.vzhilin.adapter.oracle;
+package me.vzhilin.adapter.postgres;
 
 import me.vzhilin.adapter.Converter;
 import me.vzhilin.adapter.ValueConverter;
@@ -10,13 +10,13 @@ import me.vzhilin.adapter.conv.TextConverter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public final class OracleValueConverter implements ValueConverter {
+public final class PostgresqlValueConverter implements ValueConverter {
     private final Map<String, Converter> converters = new LinkedHashMap<>();
 
-    public OracleValueConverter() {
-        bind(TextConverter.INSTANCE, "VARCHAR", "VARCHAR2", "NVARCHAR", "NVARCHAR2");
-        bind(BigDecimalConverter.INSTANCE, "NUMBER", "FLOAT", "DOUBLE", "DECIMAL");
-        bind(LongConverter.INSTANCE, "INTEGER");
+    public PostgresqlValueConverter() {
+        bind(TextConverter.INSTANCE, "bpchar", "text", "varchar", "char", "UUID");
+        bind(BigDecimalConverter.INSTANCE, "decimal", "numeric", "float", "float4", "float8", "real", "double precision", "bigserial", "money");
+        bind(LongConverter.INSTANCE, "smallint", "integer", "bigint", "serial", "int2", "int4", "int8");
     }
 
     private void bind(Converter conv, String... dataTypes) {
